04606cfd68158052c0f4171b56b5462191fddca2
Ayush Dwivedi <ayuh0091@gmail.com>
2025-06-19 12:56:04 +0530

    Add weather app

--- skycast/weather/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import weather_api, home  # Renamed to match updated function
-
-urlpatterns = [
-    path('', home, name='home'),  # Home page with weather form and history
-    path('api/weather/', weather_api, name='weather_api'),  # Correct endpoint name
-]
